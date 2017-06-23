4ac2ee140ac7114d770213a84ea668b47cafb05a
Morgan Fogarty <mofo37@gmail.com>
2017-06-22 22:30:10 -0700

    simplified server

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
+'use strict';
+
 const Hapi = require('hapi');
 
-// Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
+server.connection({ port: 3000, host: 'localhost' });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
-
-        return reply('hello world');
-    }
-});
-
-// Start the server
 server.start((err) => {
 
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+    console.log(`Server running at: ${server.info.uri}`);
 });
